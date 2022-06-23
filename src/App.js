1f45767c8434b406016982371a2af5a88123b4fa
Trajan Miller <trajanmiller324@gmail.com>
2022-06-23 19:33:47 -0400

    Reset the game when player selects an image that has already been selected

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import abstractDark from "./images/abstract-dark.jpg";
 import rocket from "./images/rocket.png";
 import beach from "./images/beach.jpg";
 import greenBackground from "./images/green-bg.jpg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Cards from "./components/Cards";
+import Scoreboard from "./components/Scoreboard";
 
 function App() {
   const [images, setImages] = useState([
@@ -16,15 +17,41 @@ function App() {
     greenBackground,
   ]);
 
+  const [imageArray, setImageArray] = useState([]);
+  const [score, setScore] = useState(0);
+
+  const resetGame = () => {
+    setImageArray([]);
+    setScore(0);
+  };
+
+  function containsDuplicates(arr) {
+    return arr.some(function (item) {
+      return arr.indexOf(item) !== arr.lastIndexOf(item);
+    });
+  }
+
+  const getImage = (e) => {
+    let newArray = [e.target.currentSrc];
+    setImageArray(imageArray.concat(newArray));
+    setScore(score + 1);
+  };
+
   const shuffle = (array) => {
     let shuffled = array.sort(() => Math.random() - 0.5);
-    console.log(shuffled);
     setImages([...shuffled]);
   };
 
+  useEffect(() => {
+    if (containsDuplicates(imageArray)) {
+      resetGame();
+    }
+  }, [imageArray]);
+
   return (
     <div className="App">
-      <Cards images={images} shuffle={shuffle} />
+      <Scoreboard score={score} />
+      <Cards images={images} shuffle={shuffle} getImage={getImage} />
     </div>
   );
 }
